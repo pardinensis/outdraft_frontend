daecfe347709f4283f0e39e0eacd231166b818e2
Tobias Schmidt <tobias.ts.schmidt@web.de>
2018-04-25 15:50:53 +0200

    editing routes; fixed main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,32 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  { path: 'hero/:internalname', component: HeroDetailComponent }
+  { path: 'hero/:internalname', component: HeroDetailComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponentComponent }
 ];
 
+
+
 @NgModule({
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+
+/*@NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
+ */
 export class AppRoutingModule {}
