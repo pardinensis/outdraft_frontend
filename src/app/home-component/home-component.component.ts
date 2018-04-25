a6c06ce46b2f280dd8093d562b88cb4352b1fc18
Tobias Schmidt <tobias.ts.schmidt@web.de>
2018-04-25 15:47:29 +0200

    adding Home-Component

---
+++ src/app/home-component/home-component.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home-component',
+  templateUrl: './home-component.component.html',
+  styleUrls: ['./home-component.component.css']
+})
+export class HomeComponentComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+  }
+
+}
