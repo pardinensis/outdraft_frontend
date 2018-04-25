3fa9e88405318ac0edbfad8ab0b839b665970949
Tobias Schmidt <tobias.ts.schmidt@web.de>
2018-04-25 15:51:37 +0200

    fixed reference to Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroService } from './hero.service';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HeroImageComponent } from './hero-image/hero-image.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroImageComponent
+    HeroImageComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
